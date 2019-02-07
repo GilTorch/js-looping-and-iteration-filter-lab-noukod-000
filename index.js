c345f51856ee41c47f4b18edd5bfc26358dedb2a
Gilbert Torchon <gilbert@noukod.com>
2019-02-07 18:27:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file
+function findMatching(array,name){
+  return array.filter(function(element){
+    return element.toLowerCase()==name.toLowerCase();
+  })
+}
+
+
+function fuzzyMatch(array,letters){
+  return array.filter((element)=>element.startsWith(letters));
+}
